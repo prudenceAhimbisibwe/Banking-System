a071218a3bfc395385d128d4027508d258c226af
PudenceAhimbisibwe <prudeahimbisibwe@gmail.com>
2022-05-30 18:31:47 +0300

    first changes

---
+++ vehicle.py
@@ -0,0 +1,15 @@
+from time import time
+
+
+class Vehicle:
+    color="white"
+    def __init__(self,name,max_speed,mileage):
+        self.name=name
+        self.max_speed=max_speed
+        self.mileage=mileage
+    def congratulation(self):
+        return f"congratulation on getting your first {self.color} {self.name} that drives with the max_speed of {self.max_speed}km/hr and covers adistance of {self.mileage} km"
+    
+    def cap(self,capacity):
+        return f"The {self.name} that has the capacity of {capacity} seats"
+    
